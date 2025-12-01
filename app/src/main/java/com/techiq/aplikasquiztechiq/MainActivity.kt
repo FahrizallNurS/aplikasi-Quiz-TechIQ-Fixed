9a0142c260c61f2d0c8860356e91e2a66c58606b
FahrizallNurS <rizalkhadam@gmail.com>
2025-12-01 19:59:27 +0700

    penambahan fragment

--- app/src/main/java/com/techiq/aplikasquiztechiq/MainActivity.kt
+++ app/src/main/java/com/techiq/aplikasquiztechiq/MainActivity.kt
@@ -6,6 +6,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,5 +23,14 @@ class MainActivity : AppCompatActivity() {
         }
 
         val username = intent.getStringExtra(LoginActivity.KEY_USERNAME)
+
+        //ambil navhost dari layout
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        //ambil controler dari host
+        val navController = findNavController(R.id.nav_host_fragment)
+        //nampilkan bottomNavigation
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        //menghubungkan bottom nav ke nav kontroler
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
